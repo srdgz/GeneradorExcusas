1e3948edf53c7baefe7766386c935857322a6ecd
srdgz <sandra.rr@icloud.com>
2023-05-28 20:15:08 +0200

    Subida inicial

---
+++ app.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    //write your code here
+    document.querySelector("#btn").addEventListener("click", function () {
+      document.querySelector("#excusa").innerHTML = generador();
+    })
+    console.log("");
+  };
+let generador = function() {
+  let pronombre = ["El", "Un"];
+  let sujeto = ["policía", "zorro", "perro", "conductor", "cómico", "ladrón"];
+  let verbo = ["cogió mi", "quitó mi", "robó mi", "se llevó mi"];
+  let objeto = ["coche", "zapato", "ordenador", "bolso"];
+  let donde = ["en mi casa", "en la calle", "en el parque", "en el súper"];
+
+  let pronombreIndex = Math.floor(Math.random() * pronombre.length);
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let verboIndex = Math.floor(Math.random() * verbo.length);
+  let objetoIndex = Math.floor(Math.random() * objeto.length);
+  let dondeIndex = Math.floor(Math.random() * donde.length);
+
+  return pronombre[pronombreIndex] + " " + sujeto[sujetoIndex] + " " + verbo[verboIndex] + " " + objeto[objetoIndex] + " " + donde[dondeIndex];
+};
